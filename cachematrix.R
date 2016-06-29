331e9c7991fae19e32823a53cc5e119c0c80b0b7
Donald Hescht <don@hescht.com>
2016-06-29 15:05:08 -0400

    Ready for grading

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverting a matrix is CPU bandwidth intensive 
+## These methods cache results so that repeated use
+## saves time and CPU bandwidth.
 
-## Write a short comment describing this function
-
+## Special matrix object that provides functions for caching 
+## an inverted matrix.  A user wishing to cache matrix inversions
+## creates this object and passes it to the casheSolve function.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinvert <- function(solve) m <<- solve
+    getinvert <- function() m
+    list(set = set, get = get,
+         setinvert = setinvert,
+         getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
-
+## The cacheSolve is used to invert a square matrix and also cache 
+## the value to save time for future uses.  This function requires the 
+## special matrix object returned from makeCacheMatrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinvert()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinvert(m)
+    m
 }
